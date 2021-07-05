1ebb0029c112fecd8a80272b5e832e2a2e05cf0c
Yekinni Ajibola <yekinnijibola@gmail.com>
2021-07-05 10:05:17 +0100

    restructured files

---
+++ linked_list.py
@@ -0,0 +1,18 @@
+class Element:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current = new_element
+        else:
+            self.head = new_element
